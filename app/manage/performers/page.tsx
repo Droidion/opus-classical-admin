9bdb85c8553b225f4fa6b677a6d050d0c2b71e22
Denis Rodionov <nicholas.hawkwood@gmail.com>
2024-04-11 11:34:55 +0300

    routes

---
+++ app/manage/performers/page.tsx
@@ -0,0 +1,7 @@
+export default async function PerformersPage() {
+  return (
+    <>
+      <div className="font-semibold text-lg mb-4">Performers</div>
+    </>
+  );
+}
--- components/Menu.tsx
+++ components/Menu.tsx
@@ -5,15 +5,19 @@ import React from "react";
 import { Divider } from "@nextui-org/react";
 import { UserButton } from "@clerk/nextjs";
 import { ThemeSwitcher } from "./ThemeSwitcher";
+import Link from "next/link";
 
 export function Menu() {
   return (
     <aside className="flex">
       <div className="mt-4 ml-4 w-40">
-        <Listbox aria-label="Actions" onAction={(key) => alert(key)}>
-          <ListboxItem key="new">Countries</ListboxItem>
-          <ListboxItem key="copy">Other Item</ListboxItem>
-          <ListboxItem key="edit">Other Item</ListboxItem>
+        <Listbox aria-label="Actions">
+          <ListboxItem key="countries">
+            <Link href="/manage/countries">Countries</Link>
+          </ListboxItem>
+          <ListboxItem key="performers">
+            <Link href="/manage/performers">Performers</Link>
+          </ListboxItem>
         </Listbox>
         <div className="flex ml-3 mt-4 w-full">
           <div className="mr-4">
